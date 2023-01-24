6e0176b185b109ac415c732f2657ff859e252525
root <Ayodele Ojo>
2023-01-24 15:07:36 -0800

    printf group project

--- _printf.c
+++ _printf.c
@@ -1,21 +1,79 @@
 #include "main.h"
-	
+
 /**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
+ * _printf - printf function.
+ * @format: variable
  *
- * Return: The number of characters printed.
+ * Return: nbytes printed.
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	va_list list;
+	unsigned int i = 0, chars_num = 0;
 
-	while (format[i])
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		write(1,&format[i],1);
-		i++;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				chars_num++;
+			}
+			else if (spec_func(format[i + 1]) != NULL)
+			{
+				chars_num += (spec_func(format[i + 1]))(list);
+				i++;
+				
+			}
+			else
+			{
+				_putchar(format[i]);
+				chars_num++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			chars_num++;
+		}
+	}
+	va_end(list);
+	return (chars_num);
+	
+}
+
+/**
+ * spec_func - Entry point
+ * @a: character.
+ *
+ * Return: 0.
+ */
+int (*spec_func(const char a))(va_list)
+{
+	print_f printf[] = {
+		{'c', printc},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	int k;
+	
+	for (k = 0; printf[k].p != '\0'; k++)
+	{
+		if (printf[k].p == a)
+		{
+			return (printf[k].func);
+		}
 	}
 
 	return (0);
-}
 
+}
--- main.h
+++ main.h
@@ -3,7 +3,30 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+int printc(va_list l);
+int print_string(va_list s);
+int (*spec_func(const char a))(va_list);
+int print_n(va_list n);
+
+
+
+/**
+ * struct types - specifier structure for printf
+ * @p: pointer to characteres specifiers
+ * @func : function pointer to print fucntions
+ */
+typedef struct types
+{
+	char p;
+	int (*func)(va_list);
+
+} print_f;
 
 #endif
